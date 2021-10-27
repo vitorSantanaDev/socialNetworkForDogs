4630cc86107e2636c847c907617f2510952f46db
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-10-27 17:27:45 -0300

    The header component was styled

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { HeaderElement } from "./styled";
+import { ReactComponent as Logo } from "../../Assets/dogs.svg";
 
 const Header = () => {
   return (
-    <HeaderElement className="container">
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login/User</Link>
+    <HeaderElement>
+      <nav className="container">
+        <Link className="logo" to="/" aria-label="Dogs">
+          <Logo />
+        </Link>
+        <Link className="login" to="/login">Login/User</Link>
       </nav>
     </HeaderElement>
   );
