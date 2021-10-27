77f09ce6443a23e1648930082bb1da1faef3e56b
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-10-27 17:01:32 -0300

    The application's Header component was created

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FooterElement } from './styled'
+
+const Footer = () => {
+  return (
+    <FooterElement>
+      <h1>Footer</h1>
+    </FooterElement>
+  )
+}
+
+export default Footer
