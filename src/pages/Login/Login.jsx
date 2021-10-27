d595e475c759721aa6ec5b3b6a3c3df9bbbd5dbd
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-10-27 18:23:51 -0300

    the nested routing within login configuration was done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router";
+import LoginCreate from "./LoginCreate/LoginCreate";
+import LoginForm from "./LoginForm/LoginForm";
+import LostPassword from "./LostPassword/LostPassword";
+import ResetPassword from "./ResetPassword/ResetPassword";
 
 const Login = () => {
   return (
     <>
-      <h3>Login</h3>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="create" element={<LoginCreate />} />
+        <Route path="lost" element={<LostPassword />} />
+        <Route path="reset" element={<ResetPassword />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
