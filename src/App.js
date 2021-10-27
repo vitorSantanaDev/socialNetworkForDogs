67a636249e10915a2aa5f5f9dc251cb3e7c6bd59
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-10-27 16:59:10 -0300

    Some routes were configured in the root of the application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Home from "./pages/Home/Home.jsx";
+import Login from "./pages/Login/Login.jsx";
 
 function App() {
   return (
     <div className="App">
+      <h1>React App</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
