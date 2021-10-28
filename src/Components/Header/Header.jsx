2319098b5de884b3d1d40f81b2c1d8a8a886f5aa
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-10-28 19:24:50 -0300

    Using context to get username

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { HeaderElement } from "./styled";
 import { ReactComponent as Logo } from "../../Assets/dogs.svg";
+import { UserContext } from "../../context/useContext";
 
 const Header = () => {
+  const { data } = useContext(UserContext);
+
   return (
     <HeaderElement>
       <nav className="container">
         <Link className="logo" to="/" aria-label="Dogs">
           <Logo />
         </Link>
-        <Link className="login" to="/login">Login/User</Link>
+        {data ? (
+          <Link className="login" to="/account">
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className="login" to="/login">
+            Login/User
+          </Link>
+        )}
       </nav>
     </HeaderElement>
   );
