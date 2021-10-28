78ca2d2ba7249fb41c8a88c9bda45d5c8ef31fdb
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-10-28 17:24:04 -0300

    I added the useForm hook to the login page components

--- src/pages/Login/LoginForm/LoginForm.jsx
+++ src/pages/Login/LoginForm/LoginForm.jsx
@@ -1,30 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import Button from "../../../Components/Forms/Button/Button";
 import Input from "../../../Components/Forms/Input/Input";
+import useForm from "../../../hooks/useForm";
 import { LoginFormSection } from "./styled";
 
 const LoginForm = () => {
-  const [username, setUsername] = useState("");
-  const [password, setpassword] = useState("");
+  const username = useForm();
+  const password = useForm();
 
   const handleClick = (event) => {
     event.preventDefault();
-    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({username, password}),
-    }).then((response) => response.json()).then((json) => console.log(json));
+    if (username.validate() && password.validate()) {
+      fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+    }
   };
 
   return (
     <LoginFormSection>
       <h1>Login</h1>
       <form onSubmit={handleClick}>
-        <Input label="User" type="text" name="username"/>
-        <Input label="Password" type="password" name="password"/>
+        <Input label="User" type="text" name="username" {...username} />
+        <Input label="Password" type="password" name="password" {...password} />
         <Button>Login</Button>
       </form>
       <Link to="/login/create">Create Acount</Link>
