d261a41862cad12a72e71a05f8a3382045aa0d3b
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-10-28 17:22:09 -0300

    New features have been added to the Input component

--- src/Components/Forms/Input/Input.jsx
+++ src/Components/Forms/Input/Input.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { InputElement, WrapperDiv } from "./styled";
 
-const Input = ({ label, type, name }) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <WrapperDiv className="wrapper">
       <label htmlFor={name}>{label}</label>
-      <InputElement id={name} name={name} type={type}/>
-      <p className="error">Error</p>
+      <InputElement
+        id={name}
+        name={name}
+        type={type}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <p className="error">{error}</p>}
     </WrapperDiv>
   );
 };
