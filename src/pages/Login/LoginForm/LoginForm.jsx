2c01b46d50d99585911006b712187c97f06ec8b0
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-10-28 19:23:59 -0300

    I started to use the userLogin function of the global context, to fetch the user's data

--- src/pages/Login/LoginForm/LoginForm.jsx
+++ src/pages/Login/LoginForm/LoginForm.jsx
@@ -1,38 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import { TOKEN_POST, USER_GET } from "../../../api/api";
 import Button from "../../../Components/Forms/Button/Button";
 import Input from "../../../Components/Forms/Input/Input";
+import { UserContext } from "../../../context/useContext";
 import useForm from "../../../hooks/useForm";
 import { LoginFormSection } from "./styled";
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
-
-  useEffect(() => {
-    const token = window.localStorage.getItem("token");
-    if (token) getUser(token);
-  }, []);
-
-  const getUser = async (token) => {
-    const { url, options } = USER_GET(token);
-    const response = await fetch(url, options);
-    const responseJson = await response.json();
-    console.log(responseJson);
-  };
+  const { userLogin } = useContext(UserContext);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (username.validate() && password.validate()) {
-      const { url, options } = TOKEN_POST({
-        username: username.value,
-        password: password.value,
-      });
-      const response = await fetch(url, options);
-      const responseJson = await response.json();
-      window.localStorage.setItem("token", responseJson.token);
-      getUser(responseJson.token);
+      userLogin(username.value, password.value);
     }
   };
 
