75a8d9a966f25d28259463986096bb4fa78a5f05
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-10-28 18:30:06 -0300

    I started using my api, for requests

--- src/pages/Login/LoginForm/LoginForm.jsx
+++ src/pages/Login/LoginForm/LoginForm.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { TOKEN_POST, USER_GET } from "../../../api/api";
 import Button from "../../../Components/Forms/Button/Button";
 import Input from "../../../Components/Forms/Input/Input";
 import useForm from "../../../hooks/useForm";
@@ -9,25 +10,36 @@ const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  const handleClick = (event) => {
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    if (token) getUser(token);
+  }, []);
+
+  const getUser = async (token) => {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const responseJson = await response.json();
+    console.log(responseJson);
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
     if (username.validate() && password.validate()) {
-      fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify({ username, password }),
-      })
-        .then((response) => response.json())
-        .then((json) => console.log(json));
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
+      });
+      const response = await fetch(url, options);
+      const responseJson = await response.json();
+      window.localStorage.setItem("token", responseJson.token);
+      getUser(responseJson.token);
     }
   };
 
   return (
     <LoginFormSection>
       <h1>Login</h1>
-      <form onSubmit={handleClick}>
+      <form onSubmit={handleSubmit}>
         <Input label="User" type="text" name="username" {...username} />
         <Input label="Password" type="password" name="password" {...password} />
         <Button>Login</Button>
