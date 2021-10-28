63c64c9334eb7e82ba7086308c9f5b4453a0dd41
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-10-28 19:21:38 -0300

    Wrapped the entire application in the context provider

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Home from "./pages/Home/Home.jsx";
 import Login from "./pages/Login/Login.jsx";
+import { UserStorage } from "./context/useContext.js";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
