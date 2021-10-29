35acdd69bc4a5176f002e8bbae858c594dd048f8
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-10-29 19:04:07 -0300

    :lipstick: Login page styles created

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,20 +5,23 @@ import LoginCreate from "./LoginCreate/LoginCreate";
 import LoginForm from "./LoginForm/LoginForm";
 import LostPassword from "./LostPassword/LostPassword";
 import ResetPassword from "./ResetPassword/ResetPassword";
+import { DivForms, LoginSection } from "./styled";
 
 const Login = () => {
-  const { login } = useContext(UserContext)
+  const { login } = useContext(UserContext);
 
-  if(login === true) return <Navigate to="/account"/>
+  if (login === true) return <Navigate to="/account" />;
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="create" element={<LoginCreate />} />
-        <Route path="lost" element={<LostPassword />} />
-        <Route path="reset" element={<ResetPassword />} />
-      </Routes>
-    </>
+    <LoginSection>
+      <DivForms>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="create" element={<LoginCreate />} />
+          <Route path="lost" element={<LostPassword />} />
+          <Route path="reset" element={<ResetPassword />} />
+        </Routes>
+      </DivForms>
+    </LoginSection>
   );
 };
 
---
+++ src/pages/Login/styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import Background from "../../Assets/login.jpg"
+
+export const LoginSection = styled.section `
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  min-height: 100vh;
+  gap: 2rem;
+
+  &::before {
+    content: "";
+    display: block;
+    background: url('${Background}') no-repeat center center;
+    background-size: cover;
+  }
+
+  @media (max-width: 40rem) {
+    grid-template-columns: 1fr;
+    &::before {
+      display: none;
+    }
+  }
+`
+export const DivForms = styled.div `
+  max-width: 30rem;
+  padding: 1rem;
+
+  @media (max-width: 40rem) {
+    max-width: 100%;
+  }
+`
