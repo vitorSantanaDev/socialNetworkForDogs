0987658786b7023f3199dd1b4ff11562cc554e72
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-01 20:46:40 -0300

    :seedling:  User page routes configured

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import { H1Element } from '../../Components/Titles/H1/H1'
+import React from "react";
+import { Route, Routes } from "react-router";
+// import { H1Element } from "../../Components/Titles/H1/H1";
+import { UserSection } from "./styled";
+import Feed from "../../Components/Feed/Feed";
+import UserPhotoPost from "./UserPhotoPost/UserPhotoPost";
+import UserHeader from "./UserHeader/UserHeader";
+import UserStatistics from "./UserStatistics/UserStatistics";
 
 const User = () => {
   return (
-    <>
-      <H1Element>Minha conta</H1Element>
-    </>
-  )
-}
+    <UserSection className="container">
+      {/* <H1Element>Minha conta</H1Element> */}
+      <UserHeader />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/post" element={<UserPhotoPost />} />
+        <Route path="/statistics" element={<UserStatistics />} />
+      </Routes>
+    </UserSection>
+  );
+};
 
-export default User
+export default User;
