86b534267aad182363a465fc8321dea57f295d85
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-01 17:42:31 -0300

    :boom:

--- src/context/useContext.js
+++ src/context/useContext.js
@@ -63,6 +63,8 @@ export const UserStorage = ({ children }) => {
         } finally {
           setLoading(false);
         }
+      } else {
+        setLogin(false)
       }
     };
     autoLogin();
---
+++ src/pages/User/User.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { H1Element } from '../../Components/Titles/H1/H1'
+
+const User = () => {
+  return (
+    <>
+      <H1Element>Minha conta</H1Element>
+    </>
+  )
+}
+
+export default User
