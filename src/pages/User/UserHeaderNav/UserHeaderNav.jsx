8270553fcd77d290db177a7bb261a874c06f0c21
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-02 17:34:22 -0300

    💄 It was styled and configured mobile menu

--- src/pages/User/UserHeaderNav/UserHeaderNav.jsx
+++ src/pages/User/UserHeaderNav/UserHeaderNav.jsx
@@ -1,35 +1,57 @@
-import React, { useContext, useState } from "react";
-import { NavElement } from "./styled";
-import { NavLink } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { MobileButton, NavElement } from "./styled";
+import { NavLink, useLocation } from "react-router-dom";
 import { UserContext } from "../../../context/useContext";
 import { ReactComponent as MinhasFotos } from "../../../Assets/feed.svg";
 import { ReactComponent as PostarFotos } from "../../../Assets/adicionar.svg";
 import { ReactComponent as Estatisticas } from "../../../Assets/estatisticas.svg";
 import { ReactComponent as Sair } from "../../../Assets/sair.svg";
+import useMedia from "../../../hooks/useMedia";
 
 const UserHeaderNav = () => {
   const { userLogout } = useContext(UserContext);
-  const { mobile, setMobile } = useState(null);
+  const mobile = useMedia("(max-width: 40rem)");
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    setMobileMenu(false)
+  }, [pathname])
+
   return (
-    <NavElement>
-      <NavLink to="/account" end>
-        <MinhasFotos />
-        {mobile && 'Minhas fotos'}
-      </NavLink>
-      <NavLink to="/account/post">
-        <PostarFotos />
-        {mobile && 'Postar'}
-      </NavLink>
-      <NavLink to="/account/statistics">
-        <Estatisticas />
-        {mobile && 'Estatisticas'}
-      </NavLink>
-      <button onClick={userLogout}>
-        {' '}
-        <Sair />
-        {mobile && 'Sair'}
-      </button>
-    </NavElement>
+    <>
+      {mobile && (
+        <MobileButton
+          aria-label="MenuMobile"
+          onClick={() => setMobileMenu(!mobileMenu)}
+          className={mobileMenu && "mobileMenuActive"}
+        ></MobileButton>
+      )}
+      <NavElement
+        className={`${mobile ? "navMobile" : "navMenu"} ${
+          mobileMenu && "mobileMenuActive"
+        }`}
+      >
+        <NavLink to="/account" end>
+          <MinhasFotos />
+          {mobile && "Minhas fotos"}
+        </NavLink>
+        <NavLink to="/account/post">
+          <PostarFotos />
+          {mobile && "Postar"}
+        </NavLink>
+        <NavLink to="/account/statistics">
+          <Estatisticas />
+          {mobile && "Estatisticas"}
+        </NavLink>
+        <button onClick={userLogout}>
+          {" "}
+          <Sair />
+          {mobile && "Sair"}
+        </button>
+      </NavElement>
+    </>
   );
 };
 
--- src/pages/User/UserHeaderNav/styled.js
+++ src/pages/User/UserHeaderNav/styled.js
@@ -1,38 +1,132 @@
 import styled from "styled-components";
 
+export const MobileButton = styled.button`
+  background-color: #eee;
+  border-radius: 0.2rem;
+  height: 40px;
+  width: 40px;
+  padding: 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid transparent;
+  transition: ease-in-out 0.2s;
+  cursor: pointer;
+
+  &::after {
+    content: "";
+    display: block;
+    width: 1.2rem;
+    height: 2px;
+    background: currentColor;
+    border-radius: 2px;
+    box-shadow: 0 6px currentColor, 0 -6px currentColor;
+  }
+
+  &:focus,
+  &:hover,
+  &.mobileMenuActive {
+    outline: none;
+    background-color: #fff;
+    box-shadow: 0 0 0 3px #fb1;
+    color: #fb1;
+  }
+
+  &.mobileMenuActive {
+    &::after {
+      transform: rotate(90deg);
+      width: 4px;
+      height: 4px;
+      box-shadow: 0 8px currentColor, 0 -8px currentColor;
+      transition: ease-in-out 0.2s;
+    }
+  }
+`;
+
 export const NavElement = styled.nav`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-gap: 1rem;
+  &.navMenu {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 1rem;
 
-  a, button {
-    background-color: #eee;
-    border-radius: .2rem;
-    height: 40px;
-    width: 40px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border: 1px solid transparent;
-    transition: ease-in-out .2s;
+    a,
+    button {
+      background-color: #eee;
+      border-radius: 0.2rem;
+      height: 40px;
+      width: 40px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border: 1px solid transparent;
+      transition: ease-in-out 0.2s;
+      cursor: pointer;
 
-    &:hover, &:focus {
-      background-color: #FFF;
-      box-shadow: 0 0 0 3px #eee;
-      border-color: #333;
-      outline: none;
-    } 
+      &:hover,
+      &:focus {
+        background-color: #fff;
+        box-shadow: 0 0 0 3px #eee;
+        border-color: #333;
+        outline: none;
+      }
 
-    &.active {
-      background: #FFF;
-      box-shadow: 0 0 0 3px #fea;
-      border-color: #fb1;
+      &.active {
+        background: #fff;
+        box-shadow: 0 0 0 3px #fea;
+        border-color: #fb1;
+      }
+    }
+
+    a {
+      &.active svg > * {
+        fill: #fb1;
+      }
     }
   }
 
-  a {
-    &.active svg > * {
-      fill: #fb1;
+  &.navMobile {
+    display: block;
+    position: absolute;
+    top: 70px;
+    right: 0px;
+    padding: 0 1rem;
+    background-color: #FFF;
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
+    border-radius: .2rem;
+    transform: translateX(-10px);
+    opacity: 0;
+    transition: ease-in-out .2s;
+    pointer-events: none;
+    
+    a, button {
+      display: flex;
+      align-items: center;
+      background: none;
+      width: 100%;
+      border: none;
+      border-bottom: 1px solid #eee;
+      padding: .5rem 0; 
+      cursor: pointer;
+
+      &:hover svg > *{
+        fill: #fb1;
+      }
+
+      svg {
+        margin-right: .5rem;
+      }
+    }
+    
+    button {
+      border-bottom: none;
+    }
+
+    &.mobileMenuActive {
+      transition: ease-in-out .3s;
+      transform: initial;
+      pointer-events: initial;
+      z-index: 1000;
+      opacity: 1;
     }
   }
 `;
