b2130a36a564c9d015c4dd0b599c9487c93ff099
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-03 19:28:14 -0300

    🌱 Some changes

--- src/Components/Feed/FeedPhotosItem/FeedPhotosItem.jsx
+++ src/Components/Feed/FeedPhotosItem/FeedPhotosItem.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { PhotoItem } from "./styled";
 
-const FeedPhotosItem = ({ photo }) => {
+const FeedPhotosItem = ({ photo, setModalPhoto }) => {
+
+  const handleClick = () => {
+    setModalPhoto(photo)
+  }
+
   return (
-    <PhotoItem>
+    <PhotoItem onClick={handleClick}>
       <img src={photo.src} alt={photo.title} />
       <span>{photo.acessos}</span>
     </PhotoItem>
