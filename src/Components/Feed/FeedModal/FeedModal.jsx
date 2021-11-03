9dae6806e24966185c7d2c91172a42bfac4bd7ac
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-03 19:26:40 -0300

    ✨ FeedModal logic created

--- src/Components/Feed/FeedModal/FeedModal.jsx
+++ src/Components/Feed/FeedModal/FeedModal.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Modal } from "./styled";
+import useFetch from "../../../hooks/useFetch";
+import { PHOTO_GET } from "../../../api/api";
+import Error from "../../Helper/Error/Error";
+import Loading from "../../Helper/Loading/Loading";
+import PhotoContent from "../../PhotoContent/PhotoContent";
 
-const FeedModal = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const FeedModal = ({ photo }) => {
+  const { data, error, loading, request } = useFetch();
 
-export default FeedModal
+  useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
+  return (
+    <Modal>
+        {error && <Error error={error}/>}
+        {loading && <Loading />}
+        {data && <PhotoContent data={data}/>}
+    </Modal>
+  );
+};
+
+export default FeedModal;
