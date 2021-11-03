26806b0c8a5941724f2939a09873a79a2ae9d262
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-03 18:37:13 -0300

    🎨 Created the loading component

---
+++ src/Components/Helper/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { H2Element } from "../../Titles/H2/H2"
+
+const Loading = () => {
+    return (
+        <H2Element>
+            Carregando...
+        </H2Element>
+    )
+}
+
+export default Loading
