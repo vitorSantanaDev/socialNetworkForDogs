c3b2e1a5e1bd384c97bd7a00cdc1623eaf99d217
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-03 19:27:40 -0300

    🌱 Some changes

--- src/Components/Feed/FeedPhotos/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos/FeedPhotos.jsx
@@ -6,14 +6,14 @@ import Error from "../../Helper/Error/Error";
 import Loading from "../../Helper/Loading/Loading";
 import { PhotosUl } from "./styled";
 
-const FeedPhotos = () => {
+const FeedPhotos = ({ setModalPhoto }) => {
   const { data, error, loading, request } = useFetch();
 
   useEffect(() => {
     const fetchPhotos = async () => {
       const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
       const { json } = await request(url, options);
-      console.log(json)
+      console.log(json);
     };
     fetchPhotos();
   }, [request]);
@@ -24,7 +24,11 @@ const FeedPhotos = () => {
     return (
       <PhotosUl className="leftAnimation">
         {data.map((photo) => (
-            <FeedPhotosItem key={photo.id} photo={photo}/>
+          <FeedPhotosItem
+            key={photo.id}
+            photo={photo}
+            setModalPhoto={setModalPhoto}
+          />
         ))}
       </PhotosUl>
     );
