32ce921aa9039066a2403109f1d6889fb4a01676
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-03 18:35:45 -0300

    💄 Added photo page style

---
+++ src/Components/Feed/FeedPhotosItem/styled.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import Views from "../../../Assets/visualizacao.svg"
+
+export const PhotoItem = styled.li`
+  display: grid;
+  border-radius: 0.2rem;
+  overflow: hidden;
+  cursor: pointer;
+
+  img {
+    grid-area: 1 / 1;
+  }
+
+  span {
+    grid-area: 1 / 1;
+    background-color: rgba(0, 0, 0, 0.3);
+    color: white;
+    font-size: 1rem;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    display: none;
+
+    &::before {
+        content: "";
+        display: inline-block;
+        width: 16px;
+        height: 10px;
+        margin-right: .25rem;
+        background: url('${Views}') no-repeat;
+    }
+  }
+
+  &:hover span {
+    display: flex;
+  }
+
+  &:nth-child(2) {
+    grid-column: 2 / 4;
+    grid-row: span 2;
+
+    @media (max-width: 40rem) {
+      grid-column: initial;
+      grid-row: initial;
+    }
+  }
+`;
