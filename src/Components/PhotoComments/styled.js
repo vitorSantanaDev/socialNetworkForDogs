9c9d95c594eac63226682b83f6a603f9972e2b41
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-04 18:45:08 -0300

    💄 Styling of the comments form

--- src/Components/PhotoComments/styled.js
+++ src/Components/PhotoComments/styled.js
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
 export const CommentList = styled.ul `
+  overflow-y: auto;
+  word-break: break-word;
+  padding: 0 2rem;
 
+  li {
+    margin-bottom: .5rem;
+    line-height: 1.2;
+  }
 ` 
