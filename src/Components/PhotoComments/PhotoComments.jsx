932c2e811c465c690eb8dac0742817621740caeb
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-04 18:11:33 -0300

    ✨ Configured all comment rendering

--- src/Components/PhotoComments/PhotoComments.jsx
+++ src/Components/PhotoComments/PhotoComments.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { UserContext } from "../../context/useContext";
+import PhotoCommentsForm from "../PhotoCommentsForm/PhotoCommentsForm";
+import { CommentList } from "./styled";
 
-const PhotoComments = ({ id, comments }) => {
-    return (
-        <>
-            
-        </>
-    )
-}
+const PhotoComments = (props) => {
+  const [comments, setComments] = useState(() => props.comments);
+  const { login } = useContext(UserContext);
+  return (
+    <>
+      <CommentList>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </CommentList>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments}/>}
+    </>
+  );
+};
 
-export default PhotoComments
+export default PhotoComments;
