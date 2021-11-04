95d22a7277408fb5704d67a9442199719d37a793
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-04 19:22:52 -0300

    ✨ Configured the delete button

---
+++ src/Components/PhotoDelete/PhotoDelete.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { PHOTO_DELETE } from "../../api/api";
+import useFetch from "../../hooks/useFetch";
+import { ButtonDelete } from "./styled";
+
+const PhotoDelete = ({ id }) => {
+  const { loading, request } = useFetch();
+
+  const handleClick = async () => {
+    const confirm = window.confirm("Tem certeza quedeseja deletar?");
+    if (confirm) {
+      const { url, options } = PHOTO_DELETE(id);
+      const { response } = await request(url, options);
+      if (response.ok) window.location.reload();
+    }
+  };
+
+  return (
+    <>
+      {loading ? (
+        <ButtonDelete disabled>Deletando...</ButtonDelete>
+      ) : (
+        <ButtonDelete onClick={handleClick}>Deletar</ButtonDelete>
+      )}
+    </>
+  );
+};
+
+export default PhotoDelete;
