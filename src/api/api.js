9e0eba6d9c3027117ad28bb5d3693619a55294f7
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-09 19:21:28 -0300

    ✨ Password recovery settings being added...

--- src/api/api.js
+++ src/api/api.js
@@ -113,3 +113,16 @@ export function GET_PHOTO(id) {
     url: `${API_URL}api/photo/${id}`
   };
 }
+
+export function LOST_PASSWORD(body) {
+  return {
+    url: `${API_URL}api/password/lost`,
+    options: {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    },
+  };
+};
--- src/pages/Login/LostPassword/LostPassword.jsx
+++ src/pages/Login/LostPassword/LostPassword.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
+import { H1Element } from "../../../Components/Titles/H1/H1";
+import Input from "../../../Components/Forms/Input/Input";
+import Button from "../../../Components/Forms/Button/Button";
+import useForm from "../../../hooks/useForm";
+import useFetch from "../../../hooks/useFetch";
+import { LOST_PASSWORD } from "../../../api/api";
+import Error from "../../../Components/Helper/Error/Error";
 
 const LostPassword = () => {
+  const login = useForm();
+  const { data, error, loading, request } = useFetch();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (login.validate()) {
+      const { url, options } = LOST_PASSWORD({
+        login: login.value,
+        url: window.location.href.replace("lost", "reset"),
+      });
+      const { json } = await request(url, options);
+      console.log(json);
+    }
+  };
+
   return (
     <>
-      <h1>Lost Password</h1>
+      <H1Element>Perdeu a senha?</H1Element>
+      {data ? (
+        <p style={{ color: "#4c1" }}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="Email / Usuário" type="text" name="login" {...login} />
+          {loading ? (
+            <Button disabled>Enviando...</Button>
+          ) : (
+            <Button>Enviar email</Button>
+          )}
+        </form>
+      )}
+      <Error error={error} />
     </>
-  )
-}
+  );
+};
 
-export default LostPassword
+export default LostPassword;
