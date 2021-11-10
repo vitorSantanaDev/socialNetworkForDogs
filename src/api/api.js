99de3cdb022ff144f71d563a576025ea930ff398
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-10 18:16:23 -0300

    ✨ Introduce news features

--- src/api/api.js
+++ src/api/api.js
@@ -140,3 +140,15 @@ export function RESET_PASSWORD(body) {
   };
 };
 
+export function GET_STATICS() {
+  return {
+    url: `${API_URL}api/stats`,
+    options: {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + window.localStorage.getItem("token"),
+      }
+    },
+  };
+};
+
