aeba3197c6e50d75d1eec310859da6800fcd7900
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-10 17:14:17 -0300

    💄 Stylized footer component

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { FooterElement } from './styled'
+import { ReactComponent as Dogs } from '../../Assets/dogs-footer.svg'
 
 const Footer = () => {
   return (
-    <FooterElement>
-      
+    <FooterElement> 
+      <Dogs />
+      <p>Dogs. Alguns direitos reservados</p>
     </FooterElement>
   )
 }
