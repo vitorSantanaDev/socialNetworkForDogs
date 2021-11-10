7d66f49e5893d781a961f8596a318f9b12d284d8
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-10 18:23:59 -0300

    ⚡️ Using React.lazy for better performance

--- src/pages/User/UserStatistics/UserStatistics.jsx
+++ src/pages/User/UserStatistics/UserStatistics.jsx
@@ -4,7 +4,7 @@ import useFetch from "../../../hooks/useFetch";
 import { GET_STATICS } from "../../../api/api";
 import Loading from "../../../Components/Helper/Loading/Loading";
 import Error from "../../../Components/Helper/Error/Error";
-import UserStatsGraphs from "../UserStatsGraphs/UserStatsGraphs";
+const UserStatsGraphs = React.lazy(() => import("../UserStatsGraphs/UserStatsGraphs"))
 
 const UserStatistics = () => {
   const { data, error, loading, request } = useFetch();
@@ -21,10 +21,10 @@ const UserStatistics = () => {
   if (error) return <Error error={error} />;
   if (data)
     return (
-      <>
+      <React.Suspense fallback={<Loading />}>
         <Head title="Estatísticas" />
         <UserStatsGraphs data={data}/>
-      </>
+      </React.Suspense>
     );
   else return null;
 };
