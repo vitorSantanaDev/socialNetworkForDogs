981aef6a972ca919eb03d41fddfac488c2c41ced
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-06-14 18:57:04 -0700

    fix: :bug: releasing url

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,2 @@
-export const API_URL = process.env.REACT_APP_API_URL;
+// export const API_URL = process.env.REACT_APP_API_URL_PRODUCTION;
+export const API_URL = "https://dogsapi.origamid.dev/json/";
