ce5afb22b230f5a32c160a44a5ea7d0ecbb6a7c2
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-06-14 18:48:39 -0700

    feat: :sparkles: creating photo service

--- src/Components/Photo/Photo.jsx
+++ src/Components/Photo/Photo.jsx
@@ -1,35 +1,38 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router";
-import { GET_PHOTO } from "../../api/api";
+
 import useFetch from "../../hooks/useFetch";
-import Error from "../Helper/Error/Error";
+
+import { GET_PHOTO_BY_ID } from "../../services/photo.service";
+
 import Head from "../Helper/Head/Head";
+import Error from "../Helper/Error/Error";
 import Loading from "../Helper/Loading/Loading";
 import PhotoContent from "../PhotoContent/PhotoContent";
+
 import { PhotoSection } from "./styled";
 
 const Photo = () => {
-
-  const { id } = useParams()
-  const { data, loading, error, request} = useFetch()
+  const { id } = useParams();
+  const { data, loading, error, request } = useFetch();
 
   useEffect(() => {
-    const { url, options } = GET_PHOTO(id)
-    request(url, options)
-  }, [id, request])
+    const { url, options } = GET_PHOTO_BY_ID(id);
+    request(url, options);
+  }, [id, request]);
 
-  if(error) return <Error error={error} />
-  if(loading) return <Loading />
-  if(data)
-  return (
-    <>
-    <Head title={data.photo.title}/>
-      <PhotoSection className="container mainContainer">
-        <PhotoContent single={true} data={data}/>
-      </PhotoSection>
-    </>
-  );
-  return null
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <>
+        <Head title={data.photo.title} />
+        <PhotoSection className="container mainContainer">
+          <PhotoContent single={true} data={data} />
+        </PhotoSection>
+      </>
+    );
+  return null;
 };
 
 export default Photo;
